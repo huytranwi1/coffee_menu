fdf187dd01c5189981ab8da349648f76aaf7db2c
huytranwi1 <huytranwii12@gmail.com>
2023-05-02 19:23:17 -0700

    new files added with content, but not done yet

---
+++ index.js
@@ -0,0 +1,20 @@
+const coffeeMenu = require("./coffee_data.js");
+
+// function printDrinks(drink){
+//     return drink.name;
+// }
+
+// console.log(coffeeMenu.forEach(printDrinks));
+
+// function drinkCostUnderFive(cost) {
+//     console.log(cost.price <= 5);
+// }
+
+// console.log(coffeeMenu.filter(drinkCostUnderFive));
+
+// function evenNumberPrice (even) {
+//     console.log(even.price %2 == 0)
+// }
+
+// const evenPrice = coffeeMenu.filter(evenNumberPrice);
+// console.log(evenPrice);
