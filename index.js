2dedd0bd96b9c0227dbf06a3ae2db2996a6cbec9
huytranwi1 <huytranwii12@gmail.com>
2023-05-04 14:08:35 -0700

    update files

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function name(items) {
 const itemsName = coffeeMenu.map(name);
 console.log(itemsName);
 
-// // Print an array of drinks that cost 5 and under.
+// // // Print an array of drinks that cost 5 and under.
 
 function lessThanFive(cost) {
   return cost.price <= 5;
@@ -17,7 +17,7 @@ function lessThanFive(cost) {
 const fiveUnder = coffeeMenu.filter(lessThanFive);
 console.log(fiveUnder);
 
-// // Print an array of drinks that are priced at an even number.
+// // // Print an array of drinks that are priced at an even number.
 
 function evenNum(evenNumber) {
   return evenNumber.price % 2 == 0;
@@ -26,35 +26,35 @@ function evenNum(evenNumber) {
 const evenPrice = coffeeMenu.filter(evenNum);
 console.log(evenPrice);
 
-// Print the total if you were to order one of every drink.
-// create a function for all price then function for total
+// // Print the total if you were to order one of every drink.
+// // create a function for all price then function for total
 
 function findPrice(totalPrice) {
-  return totalPrice.price;
+  return totalPrice.price; // list all the price
 }
 
 const priceArr = coffeeMenu.map(findPrice);
 console.log(priceArr);
 
 function total(accumulator, currentValue) {
-  return accumulator + currentValue;
+  return accumulator + currentValue; // add all the price together should be 64
 }
 
 const totalCost = priceArr.reduce(total);
 console.log(totalCost);
 
-// Print an array with all the drinks that are seasonal.
+// // Print an array with all the drinks that are seasonal.
 function seasonalArr(seasonality) {
-  return seasonality.seasonal;
+  return seasonality.seasonal; // will filter through object keys that are only true if it is season, we will get two result. affogato, cuban espresso
 }
 
 const season = coffeeMenu.filter(seasonalArr);
 console.log(season);
 
-// Print all the seasonal drinks with the words "with imported beans" after the item name. For example: "affogato with imported beans".
-
+// // Print all the seasonal drinks with the words "with imported beans" after the item name. For example: "affogato with imported beans".
+// using above function we can call the constant season to our new function.
 function name(seas) {
-  return seas.name + " with imported beans";
+  return seas.name + " with imported beans"; // should be about to show affogato and cuban espresso with its string
 }
 
 const item = season.map(name);
