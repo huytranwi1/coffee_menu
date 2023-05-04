fee0b7cec5a2870a2512635297e89532c70d352e
huytranwi1 <huytranwii12@gmail.com>
2023-05-04 13:45:16 -0700

    files added

--- coffee_data.js
+++ coffee_data.js
@@ -1,11 +1,3 @@
-// Prompt 1:
-// Clean the coffee_data file:
-
-// Checklist:
-// a. All prices should be numbers.
-// b. All items should be strings.
-// c. Objects and properties should have commas seperating them.
-// d. Connect the coffee_data.js file to the index.js file.
 
 const coffeeMenu = [
     {
--- index.js
+++ index.js
@@ -1,20 +1,61 @@
 const coffeeMenu = require("./coffee_data.js");
 
-// function printDrinks(drink){
-//     return drink.name;
-// }
+// Print an array of all the drinks on the menu.
 
-// console.log(coffeeMenu.forEach(printDrinks));
+function name(items) {
+  return items.name;
+}
 
-// function drinkCostUnderFive(cost) {
-//     console.log(cost.price <= 5);
-// }
+const itemsName = coffeeMenu.map(name);
+console.log(itemsName);
 
-// console.log(coffeeMenu.filter(drinkCostUnderFive));
+// // Print an array of drinks that cost 5 and under.
 
-// function evenNumberPrice (even) {
-//     console.log(even.price %2 == 0)
-// }
+function lessThanFive(cost) {
+  return cost.price <= 5;
+}
+const fiveUnder = coffeeMenu.filter(lessThanFive);
+console.log(fiveUnder);
 
-// const evenPrice = coffeeMenu.filter(evenNumberPrice);
-// console.log(evenPrice);
+// // Print an array of drinks that are priced at an even number.
+
+function evenNum(evenNumber) {
+  return evenNumber.price % 2 == 0;
+}
+
+const evenPrice = coffeeMenu.filter(evenNum);
+console.log(evenPrice);
+
+// Print the total if you were to order one of every drink.
+// create a function for all price then function for total
+
+function findPrice(totalPrice) {
+  return totalPrice.price;
+}
+
+const priceArr = coffeeMenu.map(findPrice);
+console.log(priceArr);
+
+function total(accumulator, currentValue) {
+  return accumulator + currentValue;
+}
+
+const totalCost = priceArr.reduce(total);
+console.log(totalCost);
+
+// Print an array with all the drinks that are seasonal.
+function seasonalArr(seasonality) {
+  return seasonality.seasonal;
+}
+
+const season = coffeeMenu.filter(seasonalArr);
+console.log(season);
+
+// Print all the seasonal drinks with the words "with imported beans" after the item name. For example: "affogato with imported beans".
+
+function name(seas) {
+  return seas.name + " with imported beans";
+}
+
+const item = season.map(name);
+console.log(item);
